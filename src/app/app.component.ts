057e1ce34dde28fe1800f79e61c39894050d1118
Louise Delbecque <delbecquelouise@gmail.com>
2026-02-19 18:16:50 +0100

    feat✨: app can display photo posts from a PhotoPost array of any size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,31 @@ import { PhotoPost } from './models/photo-post.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
-  photoPost! : PhotoPost
+  photoPosts!: PhotoPost[];
 
   ngOnInit(): void {
-    this.photoPost = new PhotoPost(
-      'Amanite mouche', 
-      'Le plus beau champignon !', 
-      'https://cdn.pixabay.com/photo/2014/11/04/08/13/fly-agaric-516281_1280.jpg', 
-      new Date(), 
-      10);
+
+    this.photoPosts = [
+      new PhotoPost(
+        'Amanite mouche', 
+        'Le plus beau champignon qu\'il ne faut surtout pas manger !', 
+        'https://cdn.pixabay.com/photo/2014/11/04/08/13/fly-agaric-516281_1280.jpg', 
+        new Date(), 
+        10),
+      new PhotoPost(
+        'Morille', 
+        'Un champignon très savoureux dans une sauce.', 
+        'https://cdn.pixabay.com/photo/2021/05/03/19/09/morels-6227065_1280.jpg', 
+        new Date(), 
+        7),
+      new PhotoPost(
+        'Champignon de paris', 
+        'Le plus plus franchouillard des champignons !', 
+        'https://cdn.pixabay.com/photo/2019/05/16/20/17/fungus-4208115_1280.jpg', 
+        new Date(), 
+        22)
+    ]
+
+    this.photoPosts[2].setLocation('Champignonnière des Carrières (EVECQUEMONT)')
   }
 }
