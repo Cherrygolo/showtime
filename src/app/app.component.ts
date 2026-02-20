cb296f6e361d65a5a329b73877c2df9c9b2fa758
Louise Delbecque <delbecquelouise@gmail.com>
2026-02-20 18:26:17 +0100

    ✨: add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { PhotoPostListComponent } from './photo-post-list/photo-post-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [
+    HeaderComponent,
     PhotoPostListComponent
   ],
   templateUrl: './app.component.html',
