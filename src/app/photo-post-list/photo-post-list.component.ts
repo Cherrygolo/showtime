4ee2da202965fc73a112b888a2e5765a79a10205
Louise Delbecque <delbecquelouise@gmail.com>
2026-02-20 18:30:42 +0100

    ♻️: externalization of photo post list generation in dedicated service class

--- src/app/photo-post-list/photo-post-list.component.ts
+++ src/app/photo-post-list/photo-post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoPost } from '../models/photo-post.model';
 import { PhotoPostComponent } from '../photo-post/photo-post.component';
+import { PhotoPostsService } from '../services/photo-posts.service';
 
 @Component({
   selector: 'app-photo-post-list',
@@ -14,30 +15,12 @@ export class PhotoPostListComponent implements OnInit {
 
   photoPosts!: PhotoPost[];
 
+  constructor(private photoPostsService: PhotoPostsService) {}
+
   ngOnInit(): void {
 
-    this.photoPosts = [
-      new PhotoPost(
-        'Amanite mouche', 
-        'Le plus beau champignon qu\'il ne faut surtout pas manger !', 
-        'https://cdn.pixabay.com/photo/2014/11/04/08/13/fly-agaric-516281_1280.jpg', 
-        new Date(), 
-        50),
-      new PhotoPost(
-        'Morille', 
-        'Un champignon très savoureux dans une sauce.', 
-        'https://cdn.pixabay.com/photo/2021/05/03/19/09/morels-6227065_1280.jpg', 
-        new Date(), 
-        111),
-      new PhotoPost(
-        'Champignon de paris', 
-        'Le plus plus franchouillard des champignons !', 
-        'https://cdn.pixabay.com/photo/2019/05/16/20/17/fungus-4208115_1280.jpg', 
-        new Date(), 
-        199)
-    ]
+    this.photoPosts = this.photoPostsService.getPhotoPosts();
 
-    this.photoPosts[2].setLocation('Champignonnière des Carrières (EVECQUEMONT)')
   }
 
 }
---
+++ src/app/services/photo-posts.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { PhotoPost } from "../models/photo-post.model";
+
+// Pour utiliser la classe comme un service
+@Injectable({
+    providedIn: 'root'
+})
+export class PhotoPostsService {
+
+    private photoPosts: PhotoPost[] = [
+      new PhotoPost(
+        'Amanite mouche', 
+        'Le plus beau champignon qu\'il ne faut surtout pas manger !', 
+        'https://cdn.pixabay.com/photo/2014/11/04/08/13/fly-agaric-516281_1280.jpg', 
+        new Date(), 
+        50),
+      new PhotoPost(
+        'Morille', 
+        'Un champignon très savoureux dans une sauce.', 
+        'https://cdn.pixabay.com/photo/2021/05/03/19/09/morels-6227065_1280.jpg', 
+        new Date(), 
+        111),
+      new PhotoPost(
+        'Champignon de paris', 
+        'Le plus plus franchouillard des champignons !', 
+        'https://cdn.pixabay.com/photo/2019/05/16/20/17/fungus-4208115_1280.jpg', 
+        new Date(), 
+        199)
+        .withLocation('Champignonnière des Carrières (EVECQUEMONT)')
+    ]
+
+    getPhotoPosts(): PhotoPost[] {
+        return [...this.photoPosts];
+    }
+
+}
